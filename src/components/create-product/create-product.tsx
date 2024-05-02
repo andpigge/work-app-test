5d175e2f245379f41abb8077d56bc4bdd730b075
Rustam <rustamaaaa@bk.ru>
2024-05-02 16:47:15 +0300

    added create-product

--- src/components/create-product/create-product.tsx
+++ src/components/create-product/create-product.tsx
@@ -1,8 +1,85 @@
 import React from "react";
+import classNames from 'classnames/bind';
+
+import { Input, Button, ButtonGroup, InputGroup, InputRightElement, InputLeftElement, Textarea } from '@chakra-ui/react'
+import { CheckIcon,  } from '@chakra-ui/icons'
 import styles from "./create-product.module.scss";
 
+const cx = classNames.bind(styles);
+
 export const CreateProduct = () => {
+  const submitForm = () => (console.log('authorization'))
+
   return (
-    <h1>Create product</h1>
+    <div className={styles.container}>
+      <form className={styles.containerFields} onSubmit={submitForm}>
+        <label>
+          <p className={cx('text', 'text-medium')}>Заголовок</p>
+          <InputGroup>
+            <Input
+              className='text-medium'
+              placeholder='Заголовок'
+            />
+            <InputRightElement>
+              <CheckIcon color='green.500' />
+            </InputRightElement>
+          </InputGroup>
+        </label>
+
+        <label>
+          <p className={cx('text', 'text-medium')}>Цена</p>
+          <InputGroup>
+            <Input
+              className='text-medium'
+              placeholder='Цена'
+            />
+            <InputRightElement>
+              <CheckIcon color='green.500' />
+            </InputRightElement>
+          </InputGroup>
+        </label>
+
+        <label>
+          <p className={cx('text', 'text-medium')}>Описание</p>
+          <InputGroup>
+            <Textarea
+              paddingRight={40}
+              className='text-medium'
+              placeholder='Описание'
+            />
+            <InputRightElement>
+              <CheckIcon color='green.500' />
+            </InputRightElement>
+          </InputGroup>
+        </label>
+
+        <label>
+          <p className={cx('text', 'text-medium')}>Путь к картинке</p>
+          <InputGroup>
+            <InputLeftElement pointerEvents='none' color='gray.300'>
+              <p className="text-medium">url</p>
+            </InputLeftElement>
+            <Input
+              className='text-medium'
+              placeholder='____'
+            />
+            <InputRightElement>
+              <CheckIcon color='green.500' />
+            </InputRightElement>
+          </InputGroup>
+        </label>
+
+        <ButtonGroup>
+          <Button
+            type="submit"
+            variant='solid'
+            colorScheme='blue'
+            className='text-medium'
+          >
+            Создать продукт
+          </Button>
+        </ButtonGroup>
+      </form>
+    </div>
   )
 }
